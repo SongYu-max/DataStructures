20270dc53e678921aa778800937b4cb72227521e
SongYu-max <1390493172@qq.com>
2022-06-23 00:39:44 +0800

    1.冒泡排序的修改（上次提交写的不是冒泡排序，已更正） 2.冒泡排序的优化（有序后不再循环）

--- src/main/java/com/atguigu/sort/BubbleSort.java
+++ src/main/java/com/atguigu/sort/BubbleSort.java
@@ -8,24 +8,38 @@ package com.atguigu.sort;
  * @createTime 2022年06月23日 00:03:00
  */
 public class BubbleSort {
+    static int count = 0;
+
     public static void main(String[] args) {
 //        int[] arr = {5, 4, 3, 2, 1};
-        int[] arr = {3, 2, 1, 4, 5};
+//        int[] arr = {3, 2, 1, 4, 5, 6};
+//        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {1, 2, 3, 4, 5, 6};
         sort(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+
+        System.out.println("次数为" + count);
     }
 
     public static void sort(int[] arr) {
         int tem = 0;
+        boolean flag = false;
         for (int i = 0; i < (arr.length - 1); i++) {
             for (int j = 0; j < arr.length - 1 - i; j++) {
+                count++;
                 if (arr[j] > arr[j + 1]) {
+                    flag = true;
                     tem = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = tem;
                 }
+            }
+            if (!flag) {
+                break;
+            } else {
+                flag = false;
             }
         }
     }
